26d2728ecf9b2ef705310cb75bbc9e20f050f189
Topher Rittenhouse <dev@litritt.com>
2024-01-11 12:13:58 -0500

    Update getRevision.sh

--- src/getRevision.sh
+++ src/getRevision.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIRECTORY=`dirname "$0"`
-rev=\"`git rev-parse --short HEAD`\"
+#rev=\"`git rev-parse --short HEAD`\"
+rev="\"3c11aa8e\""
 echo current revision $rev
 echo "#define PLUGIN_REVISION $rev" > $SCRIPT_DIRECTORY/Revision.h
 echo "#define PLUGIN_REVISION_W L$rev" >> $SCRIPT_DIRECTORY/Revision.h
